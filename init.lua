742e3d1eab13a397bdde497ce92b2d73b3c3ad1d
Nhan Nguyen <ngthanhnhanlhp2000@gmail.com>
2024-04-28 13:01:29 +0700

    add lsp

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
+vim.cmd("set smarttab")
 vim.g.mapleader = " "
 
 vim.api.nvim_create_autocmd("VimEnter", {
